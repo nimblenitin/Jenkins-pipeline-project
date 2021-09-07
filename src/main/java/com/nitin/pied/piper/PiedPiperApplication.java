87ae37b0677fd34789893619cd42a105088c6acd
nimblenitin <naidunitin1@gmail.com>
2021-09-07 15:00:37 +0000

    first commit

---
+++ src/main/java/com/nitin/pied/piper/PiedPiperApplication.java
@@ -0,0 +1,13 @@
+package com.nitin.pied.piper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiedPiperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiedPiperApplication.class, args);
+	}
+
+}
